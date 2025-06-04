6b3c08b0f46235096d577c9f81da3347ef62b342
aurelwidyanti <aurelwyt@gmail.com>
2025-06-04 13:14:10 +0700

    refactor: Update obat table structure and add Font Awesome icons for actions

--- resources/views/dokter/obat/index.blade.php
+++ resources/views/dokter/obat/index.blade.php
@@ -17,63 +17,66 @@
                             <a href="{{ route('dokter.obat.create') }}" class="btn btn-primary">Tambah Obat</a>
 
                             @if (session('status') === 'obat-created')
-                                <p
-                                    x-data="{ show: true }"
-                                    x-show="show"
-                                    x-transition
-                                    x-init="setTimeout(() => show = false, 2000)"
-                                    class="text-sm text-gray-600"
-                                >
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                                    class="text-sm text-gray-600">
                                     {{ __('Created.') }}
                                 </p>
                             @endif
                         </div>
                     </header>
 
-                    <table class="table mt-6 overflow-hidden rounded table-hover">
-                        <thead class="thead-light">
-                            <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Nama Obat</th>
-                                <th scope="col">Kemasan</th>
-                                <th scope="col">Harga</th>
-                                <th scope="col">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($obats as $obat)
+                    <div class="overflow-x-auto mt-6 rounded">
+                        <table class="table table-hover min-w-full">
+                            <thead class="thead-light">
                                 <tr>
-                                    <th scope="row" class="align-middle text-start">
-                                        {{ $loop->iteration }}
-                                    </th>
-                                    <td class="align-middle text-start">
-                                        {{ $obat->nama_obat }}
-                                    </td>
-                                    <td class="align-middle text-start">
-                                        {{ $obat->kemasan }}
-                                    </td>
-                                    <td class="align-middle text-start">
-                                        {{ 'Rp' . number_format($obat->harga, 0, ',', '.') }}
-                                    </td>
-                                    <td class="flex items-center gap-3">
-                                        {{-- Button Edit --}}
-                                        <a href="{{ route('dokter.obat.edit', $obat->id) }}" class="btn btn-secondary btn-sm">
-                                            Edit
-                                        </a>
-
-                                        {{-- Button Delete --}}
-                                        <form action="{{ route('dokter.obat.destroy', $obat->id) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm">
-                                                Delete
-                                            </button>
-                                        </form>
-                                    </td>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama Obat</th>
+                                    <th scope="col">Kemasan</th>
+                                    <th scope="col">Harga</th>
+                                    <th scope="col">Aksi</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($obats as $obat)
+                                    <tr>
+                                        <th scope="row" class="align-middle text-start">
+                                            {{ $loop->iteration }}
+                                        </th>
+                                        <td class="align-middle text-start">
+                                            {{ $obat->nama_obat }}
+                                        </td>
+                                        <td class="align-middle text-start">
+                                            {{ $obat->kemasan }}
+                                        </td>
+                                        <td class="align-middle text-start">
+                                            {{ 'Rp' . number_format($obat->harga, 0, ',', '.') }}
+                                        </td>
+                                        <td class="flex items-center gap-3">
+                                            {{-- Button Edit --}}
+                                            <a href="{{ route('dokter.obat.edit', $obat->id) }}"
+                                                class="btn btn-secondary btn-sm">
+                                                <i class="fa-solid fa-pen"></i>
+                                            </a>
+
+                                            {{-- Button Delete --}}
+                                            <form action="{{ route('dokter.obat.destroy', $obat->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm">
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div>
+                            {{ $obats->links() }}
+                        </div>
+
+                    </div>
                 </section>
             </div>
         </div>
