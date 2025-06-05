157066a39633408b31994b10e032fa1b4d2594bb
aurelwidyanti <aurelwyt@gmail.com>
2025-06-05 14:22:36 +0700

    feat: Add routes for managing appointment scheduling in pasien

--- routes/pasien.php
+++ routes/pasien.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\Pasien\JanjiPeriksaCOntroller;
+use App\Http\Controllers\Pasien\JanjiPeriksaController as PasienJanjiPeriksaController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () {
     Route::get('/dashboard', function () {
         return view('pasien.dashboard');
     })->name('pasien.dashboard');
+
+    Route::prefix('janji-periksa')->name('pasien.janji-periksa.')->group(function(){
+        Route::get('/', [JanjiPeriksaController::class, 'index'])->name('index');
+        Route::post('/', [PasienJanjiPeriksaController::class, 'store'])->name('store');
+    });
 });
