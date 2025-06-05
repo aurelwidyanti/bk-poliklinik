0f737c7c921e4dd64af6a10ce072ecadeabb91f3
aurelwidyanti <aurelwyt@gmail.com>
2025-06-05 14:22:24 +0700

    feat: Implement JanjiPeriksaController for managing appointment scheduling

---
+++ app/Http/Controllers/Pasien/JanjiPeriksaController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Pasien;
+
+use App\Http\Controllers\Controller;
+use App\Models\JanjiPeriksa;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class JanjiPeriksaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $no_rm = Auth::user()->no_rm;
+        $dokters = User::with([
+            'jadwalPeriksas' => function ($query) {
+                $query->where('status', true);
+            },
+        ])
+            ->where('role', 'dokter')
+            ->get();
+
+        return view('pasien.janji-periksa.index')->with([
+            'no_rm' => $no_rm,
+            'dokters' => $dokters,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'id_jadwal_periksa' => 'required|exists:jadwal_periksas,id',
+            'keluhan' => 'required|string',
+        ]);
+
+        $jumlahJanji = JanjiPeriksa::where('id_jadwal_periksa', $validated['id_jadwal_periksa'])->count();
+        $noAntrian = $jumlahJanji + 1;
+
+        JanjiPeriksa::create([
+            'id_pasien' => Auth::user()->id,
+            'id_jadwal_periksa' => $validated['id_jadwal_periksa'],
+            'keluhan' => $validated['keluhan'],
+            'no_antrian' => $noAntrian,
+        ]);
+
+        return Redirect::route('pasien.janji-periksa.index')->with('status', 'janji-periksa-created');
+    }
+}
