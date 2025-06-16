eafd55ddcca9b513989fc9ad7615a0ad2553bea0
aurelwidyanti <aurelwyt@gmail.com>
2025-06-16 15:55:36 +0700

    feat: Implement Riwayat Periksa functionality with index and edit views

---
+++ app/Http/Controllers/Dokter/RiwayatPeriksaController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Dokter;
+
+use App\Http\Controllers\Controller;
+use App\Models\JadwalPeriksa;
+use App\Models\Obat;
+use App\Models\Periksa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RiwayatPeriksaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $jadwalPeriksa = JadwalPeriksa::where('id_dokter', Auth::user()->id)->where('status', true)->first();
+        $periksas = Periksa::with([
+            'janjiPeriksa' => function($query) use ($jadwalPeriksa) {
+                $query->where('id_jadwal_periksa', $jadwalPeriksa->id);
+            }
+        ])->paginate(10);
+
+        return view('dokter.riwayat-periksa.index')->with([
+            'periksas' => $periksas,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $periksa = Periksa::findOrFail($id);
+
+        if ($periksa->janjiPeriksa->jadwalPeriksa->id_dokter !== Auth::user()->id) {
+            abort(403, 'Unauthorized');
+        }
+
+        $obats = Obat::all();
+        $periksa->load(['janjiPeriksa']);
+
+        return view('dokter.riwayat-periksa.edit')->with([
+            'periksa' => $periksa,
+            'obats' => $obats
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/dokter/riwayat-periksa/index.blade.php
@@ -0,0 +1,63 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Riwayat Periksa') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+            <div class="p-4 bg-white shadow-sm sm:p-8 sm:rounded-lg">
+                <section>
+                    <div class="overflow-x-auto mt-6 rounded">
+                        <table class="table table-hover min-w-full">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">No RM</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Keluhan</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($periksas as $periksa)
+                                    <tr>
+                                        <td class="align-middle text-start">
+                                            {{ $loop->iteration }}
+                                        </td>
+                                        <td class="align-middle text-start">
+                                            {{ $periksa->janjiPeriksa->pasien->no_rm }}
+                                        </td>
+
+                                        <td class="align-middle text-start">
+                                            {{ $periksa->janjiPeriksa->pasien->nama }}
+                                        </td>
+
+                                        <td class="align-middle text-start">
+                                            {{ $periksa->janjiPeriksa->keluhan }}
+                                        </td>
+                                        
+                                        
+                                        <td class="flex items-center gap-3">
+                                            {{-- Button Edit --}}
+                                            <a href="{{ route('dokter.riwayat-periksa.edit', $periksa->id) }}"
+                                                class="btn btn-warning btn-sm">
+                                                <i class="fa-solid fa-pen"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div>
+                            {{ $periksas->links() }}
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
