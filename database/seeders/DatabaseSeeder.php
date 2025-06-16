49881dcde2b6a48d51c2a6f3ab02496e7d1e9494
aurelwidyanti <aurelwyt@gmail.com>
2025-06-16 23:47:24 +0700

    feat: Add PoliSeeder to DatabaseSeeder for improved data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call([
+            PoliSeeder::class,
             DokterSeeder::class,
             ObatSeeder::class,
             JadwalPeriksaSeeder::class,
