8fef2958a80c39964b23333ef17eb292b68e7f4c
aurelwidyanti <aurelwyt@gmail.com>
2025-06-17 14:47:03 +0700

    feat: Add routes for Riwayat Periksa to manage patient history details

--- routes/pasien.php
+++ routes/pasien.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Pasien\JanjiPeriksaCOntroller;
 use App\Http\Controllers\Pasien\JanjiPeriksaController as PasienJanjiPeriksaController;
+use App\Http\Controllers\Pasien\RiwayatPeriksaController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () {
@@ -12,5 +13,11 @@ Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () 
     Route::prefix('janji-periksa')->name('pasien.janji-periksa.')->group(function(){
         Route::get('/', [JanjiPeriksaController::class, 'index'])->name('index');
         Route::post('/', [PasienJanjiPeriksaController::class, 'store'])->name('store');
+    });
+
+    Route::prefix('riwayat-periksa')->name('pasien.riwayat-periksa.')->group(function () {
+        Route::get('/', [RiwayatPeriksaController::class, 'index'])->name('index');
+        Route::get('/{id}/detail', [RiwayatPeriksaController::class, 'detail'])->name('detail');
+        Route::get('/{id}/riwayat', [RiwayatPeriksaController::class, 'riwayat'])->name('riwayat');
     });
 });
