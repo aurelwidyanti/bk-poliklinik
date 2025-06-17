dec0e29a6168e4374e172449f8fcfb12853688f2
aurelwidyanti <aurelwyt@gmail.com>
2025-06-17 14:51:21 +0700

    feat: Add Riwayat Periksa link for patient navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -40,6 +40,9 @@
                         <x-nav-link :href="route('pasien.janji-periksa.index')" :active="request()->routeIs('pasien.janji-periksa.index')">
                             {{ __('Janji Periksa') }}
                         </x-nav-link>
+                        <x-nav-link :href="route('pasien.riwayat-periksa.index')" :active="request()->routeIs('pasien.riwayat-periksa.index')">
+                            {{ __('Riwayat Periksa') }}
+                        </x-nav-link>
                     @endif
                 </div>
             </div>
@@ -120,6 +123,9 @@
             @elseif(Auth::user()->role == 'pasien')
                 <x-responsive-nav-link :href="route('pasien.dashboard')" :active="request()->routeIs('pasien.dashboard')">
                     {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('pasien.janji-periksa.index')" :active="request()->routeIs('pasien.janji-periksa.index')">
+                    {{ __('Janji Periksa') }}
                 </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('pasien.riwayat-periksa.index')" :active="request()->routeIs('pasien.riwayat-periksa.index')">
                     {{ __('Riwayat Periksa') }}
