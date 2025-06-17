cc35681d88b55f2b6e70f9c8117c0f829d8d390e
aurelwidyanti <aurelwyt@gmail.com>
2025-06-17 14:20:22 +0700

    fix: Update Riwayat Periksa view to display schedule and handle empty states

--- resources/views/dokter/riwayat-periksa/index.blade.php
+++ resources/views/dokter/riwayat-periksa/index.blade.php
@@ -20,65 +20,83 @@
                         @endif
                     </header>
 
-                    <div class="overflow-x-auto mt-6 rounded">
-                        <table class="table table-hover min-w-full">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">No</th>
-                                    <th scope="col">Tanggal Periksa</th>
-                                    <th scope="col">No RM</th>
-                                    <th scope="col">Nama</th>
-                                    <th scope="col">Keluhan</th>
-                                    <th scope="col">Biaya Periksa</th>
-                                    <th scope="col">Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($periksas as $periksa)
-                                    <tr>
-                                        <td class="align-middle text-start">
-                                            {{ $loop->iteration }}
-                                        </td>
-                                        <td class="align-middle text-start">
-                                            {{ \Carbon\Carbon::parse($periksa->tgl_periksa)->format('d-m-Y') }}
-                                        </td>
-                                        <td class="align-middle text-start">
-                                            {{ $periksa->janjiPeriksa->pasien->no_rm }}
-                                        </td>
+                    @if ($jadwalPeriksa)
+                        <header class="flex items-center justify-between">
+                            <h2 class="text-base text-gray-600">
+                                {{ __('Jadwal') }} : {{ $jadwalPeriksa->full_jadwal }}
+                            </h2>
+                        </header>
+                    @endif
 
-                                        <td class="align-middle text-start">
-                                            {{ $periksa->janjiPeriksa->pasien->nama }}
-                                        </td>
-
-                                        <td class="align-middle text-start">
-                                            {{ $periksa->janjiPeriksa->keluhan }}
-                                        </td>
-                                        <td class="align-middle text-start">
-                                            {{ 'Rp' . number_format($periksa->biaya_periksa, 0, ',', '.') }}
-                                        </td>
-                                        <td class="flex items-center gap-3">
-                                            {{-- Button Edit --}}
-                                            <a href="{{ route('dokter.riwayat-periksa.edit', $periksa->id) }}"
-                                                class="btn btn-warning btn-sm">
-                                                <i class="fa-solid fa-pen"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="7" class="align-middle text-center">
-                                            Tidak ada riwayat periksa.
-                                        </td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-
-                        <div>
-                            {{ $periksas->links() }}
+                    @if (!$jadwalPeriksa)
+                        <div class="text-center py-12">
+                            <h3 class="text-lg font-medium uppercase text-gray-900 mb-2">Belum Ada Jadwal Periksa</h3>
+                            <p class="text-gray-600">Anda belum memiliki jadwal periksa yang aktif. Silakan tambah atau
+                                aktifkan
+                                jadwal periksa anda.
+                            </p>
                         </div>
+                    @else
+                        <div class="overflow-x-auto mt-6 rounded">
+                            <table class="table table-hover min-w-full">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th scope="col">No</th>
+                                        <th scope="col">Tanggal Periksa</th>
+                                        <th scope="col">No RM</th>
+                                        <th scope="col">Nama</th>
+                                        <th scope="col">Keluhan</th>
+                                        <th scope="col">Biaya Periksa</th>
+                                        <th scope="col">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($periksas as $periksa)
+                                        <tr>
+                                            <td class="align-middle text-start">
+                                                {{ $loop->iteration }}
+                                            </td>
+                                            <td class="align-middle text-start">
+                                                {{ \Carbon\Carbon::parse($periksa->tgl_periksa)->format('d-m-Y') }}
+                                            </td>
+                                            <td class="align-middle text-start">
+                                                {{ $periksa->janjiPeriksa->pasien->no_rm }}
+                                            </td>
 
-                    </div>
+                                            <td class="align-middle text-start">
+                                                {{ $periksa->janjiPeriksa->pasien->nama }}
+                                            </td>
+
+                                            <td class="align-middle text-start">
+                                                {{ $periksa->janjiPeriksa->keluhan }}
+                                            </td>
+                                            <td class="align-middle text-start">
+                                                {{ 'Rp' . number_format($periksa->biaya_periksa, 0, ',', '.') }}
+                                            </td>
+                                            <td class="flex items-center gap-3">
+                                                {{-- Button Edit --}}
+                                                <a href="{{ route('dokter.riwayat-periksa.edit', $periksa->id) }}"
+                                                    class="btn btn-warning btn-sm">
+                                                    <i class="fa-solid fa-pen"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="7" class="align-middle text-center">
+                                                Tidak ada riwayat periksa.
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+
+                            <div>
+                                {{ $periksas->links() }}
+                            </div>
+
+                        </div>
+                    @endif
                 </section>
             </div>
         </div>
