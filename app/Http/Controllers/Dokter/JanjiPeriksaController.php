e4ed50ba39a78636666e56cb95a604f294241d00
aurelwidyanti <aurelwyt@gmail.com>
2025-06-18 14:49:10 +0700

    remove unused show and create methods from JadwalPeriksaController and JanjiPeriksaController

--- app/Http/Controllers/Dokter/JanjiPeriksaController.php
+++ app/Http/Controllers/Dokter/JanjiPeriksaController.php
@@ -38,14 +38,6 @@ class JanjiPeriksaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create(string $id)
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request, string $id)
@@ -96,29 +88,5 @@ class JanjiPeriksaController extends Controller
             'janjiPeriksa' => $janjiPeriksa,
             'obats' => $obats
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
